7757ce34618d58193fe3a0efe43e4f4d721c0d1c
samyak <samyakcodes@gmail.com>
2025-04-30 14:50:54 +0530

    bfs traversal 2

--- Graphs/bfsTraversal.java
+++ Graphs/bfsTraversal.java
@@ -17,13 +17,11 @@ public class bfsTraversal {
     public static void bfs(ArrayList<Edge>[] graph){
         Queue<Integer> q = new LinkedList<>();
         boolean[] vis = new boolean[graph.length];
-
         q.add(0);
-
         while(!q.isEmpty()){
             int curr = q.remove();
-            if(!vis[curr]){
-                System.out.print(curr + " ");
+            if(!vis[curr]){  
+                System.out.print(curr + " ");  
                 vis[curr] = true;
             }
             for(int i=0; i<graph[curr].size(); i++){
